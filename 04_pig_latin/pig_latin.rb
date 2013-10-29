772e3a59ceb5dc8bb6c4927bbc0e272953ba7c07
Moin Virani <moin.virani@gmail.com>
2013-10-29 16:56:06 -0400

    Pig_Latin tests passing

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,12 +1,11 @@
 def translate(word)
-	vowels = ["a, e, i, o, u"]
+	vowels = ["a", "e", "i", "o", "u"]
 	if vowels.include?(word[0])
 		return word + "ay"
 	elsif !vowels.include?(word[0]) && !vowels.include?(word[1])
 		return word[2..-1] +word[0..1] + "ay"
 	else
-		return word[1..-1] + word[0] + "ay"
-			
+		return word[1..-1] + word[0] + "ay"		
 		end
 end
 
